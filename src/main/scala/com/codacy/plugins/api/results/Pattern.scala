3f2e0f575f86bb6d5620a4a71074ba3972a1d9ff
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2019-10-23 19:10:35 +0200

    Proposal refinement

--- src/main/scala/com/codacy/plugins/api/results/Pattern.scala
+++ src/main/scala/com/codacy/plugins/api/results/Pattern.scala
@@ -38,19 +38,18 @@ object Pattern {
 
   object Category {
     sealed trait SecuritySubcategory
-    object SecuritySubcategory {
-      case object Injection extends SecuritySubcategory
-      case object BrokenAuth extends SecuritySubcategory
-      case object SensitiveData extends SecuritySubcategory
-      case object XXE extends SecuritySubcategory
-      case object BrokenAccess extends SecuritySubcategory
-      case object Misconfiguration extends SecuritySubcategory
-      case object XSS extends SecuritySubcategory
-      case object BadDeserialization extends SecuritySubcategory
-      case object VulnerableComponent extends SecuritySubcategory
-      case object NoLogging extends SecuritySubcategory
-    }
-
+    
+    case object Injection extends SecuritySubcategory
+    case object BrokenAuth extends SecuritySubcategory
+    case object SensitiveData extends SecuritySubcategory
+    case object XXE extends SecuritySubcategory
+    case object BrokenAccess extends SecuritySubcategory
+    case object Misconfiguration extends SecuritySubcategory
+    case object XSS extends SecuritySubcategory
+    case object BadDeserialization extends SecuritySubcategory
+    case object VulnerableComponent extends SecuritySubcategory
+    case object NoLogging extends SecuritySubcategory
+    
     case class Security(subcategory: Option[SecuritySubcategory]) extends Category
     case object CodeStyle extends Category
     case object ErrorProne extends Category
@@ -63,8 +62,25 @@ object Pattern {
     case object Duplication extends Category
     case object Documentation extends Category
   }
+}
 
+object Serializers {
   import play.api.libs.json._
+  import Pattern._
+
+  implicit val InjectionFormat = Json.format[Category.Injection.type]
+  implicit val BrokenAuthFormat = Json.format[Category.BrokenAuth.type]
+  implicit val SensitiveDataFormat = Json.format[Category.SensitiveData.type]
+  implicit val XXEFormat = Json.format[Category.XXE.type]
+  implicit val BrokenAccessFormat = Json.format[Category.BrokenAccess.type]
+  implicit val MisconfigurationFormat = Json.format[Category.Misconfiguration.type]
+  implicit val XSSFormat = Json.format[Category.XSS.type]
+  implicit val BadDeserializationFormat = Json.format[Category.BadDeserialization.type]
+  implicit val VulnerableComponentFormat = Json.format[Category.VulnerableComponent.type]
+  implicit val NoLoggingFormat = Json.format[Category.NoLogging.type]
+
+  implicit val subcategoryFormat = Json.format[Category.SecuritySubcategory]
+
   implicit val specificationFormat = new Reads[Specification] {
     def reads(json: JsValue): JsResult[Specification] = {
       val category: JsResult[Category] =
@@ -73,27 +89,12 @@ object Pattern {
             value match {
               case "Security" =>
                 val subcategory = json \ "subcategory"
-                val eitherCategory: Option[JsResult[Category.SecuritySubcategory]] = subcategory.toOption.map {
-                  case JsString(value) =>
-                    value match {
-                      case "Injection"           => JsSuccess(Category.SecuritySubcategory.Injection)
-                      case "BrokenAuth"          => JsSuccess(Category.SecuritySubcategory.BrokenAuth)
-                      case "SensitiveData"       => JsSuccess(Category.SecuritySubcategory.SensitiveData)
-                      case "XXE"                 => JsSuccess(Category.SecuritySubcategory.XXE)
-                      case "BrokenAccess"        => JsSuccess(Category.SecuritySubcategory.BrokenAccess)
-                      case "Misconfiguration"    => JsSuccess(Category.SecuritySubcategory.Misconfiguration)
-                      case "XSS"                 => JsSuccess(Category.SecuritySubcategory.XSS)
-                      case "BadDeserialization"  => JsSuccess(Category.SecuritySubcategory.BadDeserialization)
-                      case "VulnerableComponent" => JsSuccess(Category.SecuritySubcategory.VulnerableComponent)
-                      case "NoLogging"           => JsSuccess(Category.SecuritySubcategory.NoLogging)
-                      case _                     => JsError("Wrong subcategory")
-                    }
-                  case _ => JsError("Subcategory should be a string")
-                }
-                val res: JsResult[Option[Category.SecuritySubcategory]] = eitherCategory match {
-                  case None                    => JsSuccess(None)
-                  case Some(JsSuccess(sub, _)) => JsSuccess(Some(sub))
-                  case Some(JsError(err))      => JsError(err)
+                val res = subcategory match {
+                  case JsDefined(value) => value.asOpt[Category.SecuritySubcategory] match {
+                    case Some(sub) => JsSuccess(Some(sub))
+                    case None => JsError("Wrong subcategory")
+                  }
+                  case _: JsUndefined => JsSuccess(None) 
                 }
                 res.map(Category.Security.apply)
               case "CodeStyle"         => JsSuccess(Category.CodeStyle)
